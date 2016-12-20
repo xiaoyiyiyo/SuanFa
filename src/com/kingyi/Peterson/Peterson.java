8c7d8faec6d7f4166829db74e77bbba7a6d38293
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2016-12-20 15:51:39 +0800

    Create Peterson.java

---
+++ src/com/kingyi/Peterson/Peterson.java
@@ -0,0 +1,38 @@
+package testtest;
+//www.xuebuyuan.com/2000074.html
+
+public class Peterson implements Runnable{
+	//主观地表示某一个进程是否希望使用资源
+	private static boolean[] in = {false,false};
+	//客观地表示哪一个进程有权使用进程
+	private static volatile int turn = 0;
+	private final int id;
+	
+	public Peterson(int i){
+		id = i;
+	}
+	
+	private int other(){
+		return id == 0 ? 1 : 0;
+	}
+	
+	@Override
+	public void run() {
+		//表示本进程希望使用资源
+		in[id] = true;
+		//谦让，把使用进程的权限让给对方
+		turn = other();
+		//如果对方进程想使用资源，且有使用权限
+		while(in[other()] && turn == other()){
+			System.out.println("["+id+"] - waiting...");
+		}
+		System.out.println("["+id+"] - working ( [" + other() + "]" + ((!in[other()])?"do other":"wait to turn")+")");
+		//本进程使用完资源，表示不再想用资源
+		in[id] = false;
+	}
+	
+	public static void main(String[] args) {
+		new Thread(new Peterson(0),"Thread-0").start();
+		new Thread(new Peterson(1),"Thread-1").start();
+	}
+}
