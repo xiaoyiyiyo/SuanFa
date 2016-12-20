786592ecd033df802fbf3dfc82dc2bc875310c31
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2016-12-20 15:58:39 +0800

    Update Peterson.java

--- src/com/kingyi/Peterson/Peterson.java
+++ src/com/kingyi/Peterson/Peterson.java
@@ -2,9 +2,9 @@ package testtest;
 //www.xuebuyuan.com/2000074.html
 
 public class Peterson implements Runnable{
-	//主观地表示某一个进程是否希望使用资源
+	//主观地表示某一个线程是否希望使用资源
 	private static boolean[] in = {false,false};
-	//客观地表示哪一个进程有权使用进程
+	//客观地表示哪一个线程有权使用进程
 	private static volatile int turn = 0;
 	private final int id;
 	
@@ -18,16 +18,16 @@ public class Peterson implements Runnable{
 	
 	@Override
 	public void run() {
-		//表示本进程希望使用资源
+		//表示本线程希望使用资源
 		in[id] = true;
-		//谦让，把使用进程的权限让给对方
+		//谦让，把使用线程的权限让给对方
 		turn = other();
-		//如果对方进程想使用资源，且有使用权限
+		//如果对方线程想使用资源，且有使用权限
 		while(in[other()] && turn == other()){
 			System.out.println("["+id+"] - waiting...");
 		}
 		System.out.println("["+id+"] - working ( [" + other() + "]" + ((!in[other()])?"do other":"wait to turn")+")");
-		//本进程使用完资源，表示不再想用资源
+		//本线程使用完资源，表示不再想用资源
 		in[id] = false;
 	}
 	
