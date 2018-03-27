023b6e130a4d05e1997f34e13de0f808d7287595
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2018-03-27 21:10:48 +0800

    socket dead lock

---
+++ src/com/kingyi/deadlock/socket_dead/Client.java
@@ -0,0 +1,23 @@
+package com.kingyi.deadlock.socket_dead;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by xiaoyiyiyo on 2018/3/27.
+ */
+public class Client {
+    public static void main(String[] args) throws IOException {
+        testDead();
+    }
+
+    public static void testDead() throws IOException {
+        int count = 0;
+        Socket s = new Socket("localhost", 6060);
+        while (true) {
+            s.getOutputStream().write(new byte[1024 * 8]);
+            s.getInputStream().read();
+            System.out.println("client: " + ++count);
+        }
+    }
+}
---
+++ src/com/kingyi/deadlock/socket_dead/Server.java
@@ -0,0 +1,26 @@
+package com.kingyi.deadlock.socket_dead;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by xiaoyiyiyo on 2018/3/27.
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+        testDead();
+    }
+
+    public static void testDead() throws IOException {
+        int count = 0;
+        ServerSocket ss = new ServerSocket(6060);
+        Socket s = ss.accept();
+        while (true) {
+            s.getOutputStream().write(new byte[1024 * 8]);
+            s.getInputStream().read();
+            System.out.println("server: " + ++count);
+        }
+    }
+
+}
